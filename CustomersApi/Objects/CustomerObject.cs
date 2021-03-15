b1e4dd4ba54c12906c47c9c524942196eb6e1098
Jonáš Kowalczyk <jonaskowalczyk12@gmail.com>
2021-03-16 00:05:17 +0100

    Rename of Customer object class file

--- CustomersApi/Objects/CustomerObject.cs
+++ CustomersApi/Objects/CustomerObject.cs
@@ -8,7 +8,7 @@ namespace CustomersApi.Objects
         public int Id { get; set; }
 
         [Required]
-        [MaxLength(100)]
+        [MaxLength(200)]
         public string Name { get; set; }
 
         [Required]
